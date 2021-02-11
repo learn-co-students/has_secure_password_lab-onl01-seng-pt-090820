99f68627323036a47bf3e17ad34e1be0182198ff
DavidRMorphew <morphewd@umich.edu>
2021-02-11 14:29:38 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def login
+    if !session[:user_id].blank?
+      redirect_to welcome_path
+    end
+    @user = User.new
+  end
+
+  def create
+    if user = User.find_by(name: params[:user][:name])
+      if user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+      else
+        redirect_to login_path, alert: "Wrong password."
+      end
+    else
+      redirect_to login_path, alert: "User not found."
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get '/', to: "welcome#welcome", as: "welcome"
+
+  get '/login', to: "sessions#login", as: "login"
+
+  post '/create', to: "sessions#create", as: "create_session"
+
+  post '/logout', to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
 end
